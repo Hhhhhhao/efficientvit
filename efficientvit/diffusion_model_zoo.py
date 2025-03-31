65ddd21b9caf1124a3d9b19b6f8e950edce364a5
Hao Chen <haoc3@andrew.cmu.edu>
2025-03-31 04:43:17 +0000

    update eval

--- efficientvit/diffusion_model_zoo.py
+++ efficientvit/diffusion_model_zoo.py
@@ -7,6 +7,7 @@ from omegaconf import OmegaConf
 from efficientvit.diffusioncore.evaluator import Evaluator, EvaluatorConfig
 from efficientvit.diffusioncore.models.dit import dc_ae_dit_xl_in_512px
 from efficientvit.diffusioncore.models.uvit import (
+    maetok_usit_2b_in_512px,
     dc_ae_usit_2b_in_512px,
     dc_ae_usit_h_in_512px,
     dc_ae_uvit_2b_in_512px,
@@ -69,6 +70,14 @@ REGISTERED_DCAE_DIFFUSION_MODEL: dict[str, tuple[Callable, str, float, int, Opti
         32,
         None,
     ),
+    "maetok-b-128-usit-2b-in-512px": (
+        maetok_usit_2b_in_512px,
+        "MAETok/maetok-b-128-512",
+        1.0640852
+        1.1726909,
+        32,
+        None,
+    ),
     ################################################################################
     "dc-ae-f64c128-in-1.0-uvit-h-in-512px": (
         dc_ae_uvit_h_in_512px,
@@ -109,6 +118,13 @@ def create_dc_ae_diffusion_model_cfg(name: str, pretrained_path: Optional[str] =
     cfg: EvaluatorConfig = OmegaConf.to_object(OmegaConf.merge(OmegaConf.structured(EvaluatorConfig), cfg))
     return cfg
 
+def create_maetok_diffusion_model_cfg(name: str, pretrained_path: Optional[str] = None) -> EvaluatorConfig:
+    diffusion_cls, ae_name, bias_factor, scaling_factor, in_channels, default_pt = REGISTERED_DCAE_DIFFUSION_MODEL[name]
+    pretrained_path = default_pt if pretrained_path is None else pretrained_path
+    cfg_str = diffusion_cls(ae_name, bias_factor, scaling_factor, in_channels, pretrained_path)
+    cfg = OmegaConf.from_dotlist(cfg_str.split(" ") + ["run_dir=tmp"])
+    cfg: EvaluatorConfig = OmegaConf.to_object(OmegaConf.merge(OmegaConf.structured(EvaluatorConfig), cfg))
+    return cfg
 
 class DCAE_Diffusion_HF(nn.Module, PyTorchModelHubMixin):
     def __init__(self, model_name: str):
@@ -118,5 +134,18 @@ class DCAE_Diffusion_HF(nn.Module, PyTorchModelHubMixin):
         self.autoencoder, self.diffusion_model, self.scaling_factor = (
             evaluator.autoencoder,
             evaluator.network,
+            cfg.scaling_factor,
+        )
+
+
+class MAE_Diffusion_HF(nn.Module, PyTorchModelHubMixin):
+    def __init__(self, model_name: str):
+        super().__init__()
+        cfg = create_maetok_diffusion_model_cfg(model_name)
+        evaluator = Evaluator(cfg)
+        self.autoencoder, self.diffusion_model, self.bias_factor, self.scaling_factor = (
+            evaluator.autoencoder,
+            evaluator.network,
+            cfg.bias_factor,
             cfg.scaling_factor,
         )
